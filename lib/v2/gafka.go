cdc51a86edef45659ab7bd924f6cc9c16c828be8
zikwall <andrey.kapitonov.96@gmail.com>
2020-10-16 23:18:23 +0300

    V2

--- lib/v2/gafka.go
+++ lib/v2/gafka.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"math"
 	"math/rand"
 	"sync"
+	"time"
 )
 
 type (
@@ -21,13 +23,16 @@ type (
 		cancel  context.CancelFunc
 
 		// topic:group:unique -> message channel
-		consumers map[string]map[string]map[int]chan string
+		consumers map[string]map[string]map[int]chan []string
 
-		// topic:group -> offset for consumer group
-		offsets map[string]map[string]uint64
+		// topic:group:partition -> offset for consumer group
+		offsets map[string]map[string]map[int]uint64
 
 		// topic:partitions
 		topics map[string]int
+
+		// topic:group:partition:unique
+		partitionListeners map[string]map[string]map[int][]int
 
 		// topic -> message pool
 		messagePools map[string]chan string
@@ -41,18 +46,20 @@ func Gafka(ctx context.Context, topics []Topic) *GafkaEmitter {
 	gf := new(GafkaEmitter)
 	gf.context, gf.cancel = context.WithCancel(ctx)
 
-	gf.consumers = map[string]map[string]map[int]chan string{}
-	gf.offsets = map[string]map[string]uint64{}
+	gf.consumers = map[string]map[string]map[int]chan []string{}
+	gf.offsets = map[string]map[string]map[int]uint64{}
 	gf.topics = make(map[string]int, len(topics))
 	gf.messages = map[string]map[int][]string{}
 	gf.messagePools = map[string]chan string{}
+	gf.partitionListeners = map[string]map[string]map[int][]int{}
 
 	for _, topic := range topics {
 		gf.topics[topic.Name] = topic.Partitions
 		gf.unsafeCreateTopicPartitions(topic.Name, topic.Partitions)
-		gf.consumers[topic.Name] = map[string]map[int]chan string{}
-		gf.offsets[topic.Name] = map[string]uint64{}
+		gf.consumers[topic.Name] = map[string]map[int]chan []string{}
+		gf.offsets[topic.Name] = map[string]map[int]uint64{}
 		gf.messagePools[topic.Name] = make(chan string)
+		gf.partitionListeners[topic.Name] = map[string]map[int][]int{}
 	}
 
 	go gf.listen()
@@ -120,7 +127,7 @@ func (gf *GafkaEmitter) Publish(topic string, message string) error {
 	return nil
 }
 
-func (gf *GafkaEmitter) Subscribe(c context.Context, topic, group string, handle func(string)) (error, func()) {
+func (gf *GafkaEmitter) Subscribe(c context.Context, topic, group string, handle func([] string)) (error, func()) {
 	ctx, cancel := context.WithCancel(c)
 
 	gf.mu.RLock()
@@ -135,7 +142,7 @@ func (gf *GafkaEmitter) Subscribe(c context.Context, topic, group string, handle
 
 	// temporary unique consumer id
 	uniqId := rand.Intn(1000000000-1) + 1
-	channel := make(chan string, 10)
+	channel := make(chan []string, 10)
 
 	// global one lock
 	gf.mu.Lock()
@@ -143,19 +150,25 @@ func (gf *GafkaEmitter) Subscribe(c context.Context, topic, group string, handle
 	// check group already exist
 	if _, ok := gf.consumers[topic][group]; !ok {
 		// add new cunsumer group for topic
-		gf.consumers[topic][group] = map[int]chan string{}
+		gf.consumers[topic][group] = map[int]chan []string{}
 	}
 
 	// check exist consumer in consumer group
 	if _, ok := gf.consumers[topic][group][uniqId]; !ok {
 		// add new consumer to consumer group
-		gf.consumers[topic][group][uniqId] = make(chan string)
+		gf.consumers[topic][group][uniqId] = make(chan []string)
 	}
 
 	// check offsets for consumer group
 	if _, ok := gf.offsets[topic][group]; !ok {
 		// offsets not exist -> zero
-		gf.offsets[topic][group] = 0
+		gf.offsets[topic][group] = map[int]uint64{}
+	}
+
+	// check partition listeners
+	if _, ok := gf.partitionListeners[topic][group]; !ok {
+		// create new listeners
+		gf.partitionListeners[topic][group] = map[int][]int{}
 	}
 
 	gf.consumers[topic][group][uniqId] = channel
@@ -171,24 +184,124 @@ func (gf *GafkaEmitter) Subscribe(c context.Context, topic, group string, handle
 			gf.mu.Unlock()
 		}()
 
-		defer close(channel)
 		defer fmt.Println("Cancel consumer for topic: ", topic, " group: ", group, " id: ", uniqId)
 
-		// todo read from storage
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case message := <-gf.messagePools[topic]:
-				handle(message)
+		batchSize := 10
+		listenPart := []int{}
+
+		listenPart = []int{}
+
+		defer func() {
+			gf.mu.Lock()
+			defer gf.mu.Unlock()
+
+			remove := func (s []int, i int) []int {
+				s[i] = s[len(s)-1]
+				return s[:len(s)-1]
+			}
+
+			// remove all partition listeners
+			for _, part := range listenPart {
+				for k, listener := range gf.partitionListeners[topic][group][part] {
+					if listener == uniqId {
+						gf.partitionListeners[topic][group][part] = remove(gf.partitionListeners[topic][group][part], k)
+
+						fmt.Println("REMOVE LISTENER ", k, uniqId, gf.partitionListeners[topic][group][part])
+					}
+				}
+			}
+		}()
+
+	BALANCE:
+		gf.mu.Lock()
+
+		listenPart = []int{}
+
+		countConsumerInGroup := len(gf.consumers[topic][group])
+		partOneConsumer := float64(len(gf.messages[topic])) / float64(len(gf.consumers[topic][group]))
+
+		fmt.Println(partOneConsumer)
+
+		// 4 + 0.5 => 5
+		// 3.9 + 0.5 => 4
+		per := int(math.Round(partOneConsumer + 0.49))
+
+		fmt.Println(per)
+
+		fmt.Println("TOPIC ", topic, " GROUP ", group , gf.partitionListeners[topic][group])
+
+		for part := 1; part <= len(gf.messages[topic]); part++ {
+			listeners := gf.partitionListeners[topic][group][part]
+			count := len(listeners)
+
+			if count < per {
+				count++
+
+				listenPart = append(listenPart, part)
+				gf.partitionListeners[topic][group][part] = append(gf.partitionListeners[topic][group][part], uniqId)
 			}
 		}
+
+		fmt.Println("TOPIC ", topic, " GROUP ", group , gf.partitionListeners[topic][group])
+
+		gf.mu.Unlock()
+
+		fmt.Println("TOPIC ", topic, " GROUP ", group , "LISTEN PARTS ", listenPart)
+
+	RETRY:
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+
+		gf.mu.Lock()
+		if countConsumerInGroup != len(gf.consumers[topic][group]) {
+			gf.mu.Unlock()
+
+			fmt.Println("BALANCE")
+
+			goto BALANCE
+		}
+
+		for _, part := range listenPart {
+			offset := gf.unsafePeekOffsetForConsumerGroup(topic, group, part)
+			count := gf.unsafePeekPartitionLength(topic, part)
+			max := offset + uint64(batchSize)
+
+			if max > count {
+				max = count
+			}
+
+			messages := gf.messages[topic][part][offset:max]
+
+			gf.offsets[topic][group][part] = max
+
+			if len(messages) > 0 {
+				handle(messages)
+			}
+		}
+
+		gf.mu.Unlock()
+
+		time.Sleep(time.Millisecond * 1000)
+
+		fmt.Println("RETRY")
+		goto RETRY
 	}()
 
 	// unsubscribe callback
 	return nil, func() {
 		cancel()
 	}
+}
+
+func (gf GafkaEmitter) unsafePeekOffsetForConsumerGroup(topic, group string, part int) uint64 {
+	return gf.offsets[topic][group][part]
+}
+
+func (gf GafkaEmitter) unsafePeekPartitionLength(topic string, partition int) uint64 {
+	return uint64(len(gf.messages[topic][partition]))
 }
 
 func (gf *GafkaEmitter) unsafeCreateTopicPartitions(topic string, partitions int) {
--- main_v2.go
+++ main_v2.go
@@ -15,8 +15,8 @@ func main() {
 		{Name: "testTopicName", Partitions: 4},
 	})
 
-	err, unsubscribe := gafka.Subscribe(ctx, "testTopicName", "group1", func(message string) {
-		log.Println("First ", message)
+	err, unsubscribe := gafka.Subscribe(ctx, "testTopicName", "group1", func(messages []string) {
+		log.Println("First ", messages)
 	})
 
 	defer unsubscribe()
@@ -25,15 +25,44 @@ func main() {
 		log.Fatal(err)
 	}
 
-	err, unsubscribe2 := gafka.Subscribe(ctx, "testTopicName", "group1", func(message string) {
-		log.Println("Second ", message)
+	err, unsubscribe2 := gafka.Subscribe(ctx, "testTopicName", "group1", func(messages []string) {
+		log.Println("Second ", messages)
 	})
 
-	defer unsubscribe2()
+	go func() {
+		time.Sleep(5 * time.Second)
+		unsubscribe2()
+	}()
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	err, unsubscribe3 := gafka.Subscribe(ctx, "testTopicName", "group2", func(messages []string) {
+		log.Println("Third ", messages)
+	})
+
+	defer unsubscribe3()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/*go func() {
+		time.Sleep(3 * time.Second)
+
+		err, unsubscribe := gafka.Subscribe(ctx, "testTopicName", "group1", func(messages []string) {
+			log.Println("Four ", messages)
+		})
+
+		defer unsubscribe()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		time.Sleep(100 * time.Second)
+	}()*/
 
 	for i := 0; i <= 100; i++ {
 		if err := gafka.Publish("testTopicName", fmt.Sprintf("message #%d", i)); err != nil {
