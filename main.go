2795924b9b4dfffae7ff1cf4fa98509d9a8dcab3
zikwall <andrey.kapitonov.96@gmail.com>
2020-10-22 19:09:05 +0300

    Add Cli app

--- main.go
+++ main.go
@@ -2,197 +2,43 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/goavengers/gafka/lib"
+	"github.com/urfave/cli/v2"
 	"log"
+	"os"
 	"time"
 )
 
 func main() {
-	topic := "testTopicName"
-
-	ctx := context.Background()
-
-	bootstrapTopics := []lib.Topic{
-		{Name: "testTopicName", Partitions: 6},
+	application := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "bind-address",
+				Usage:    "IP и порт сервера, например: 0.0.0.0:3000",
+				Required: true,
+				EnvVars:  []string{"GAFKA_BIND_ADDRESS"},
+			},
+		},
 	}
 
-	gafka := lib.Gafka(ctx, lib.Configuration{
-		BatchSize:       10,
-		ReclaimInterval: time.Second * 2,
-		Topics:          bootstrapTopics,
-	})
+	application.Action = func(c *cli.Context) error {
+		ctx := context.Background()
 
-	err, unsubscribe := gafka.Subscribe(lib.SubscribeConf{
-		Topic: topic,
-		Group: "group1",
-		Handler: func(message lib.ReceiveMessage) {
-			//log.Println("First")
-			//log.Println(message.Topic)
-			//log.Println(message.Partition)
-			//log.Println(message.Messages)
-		},
-	})
+		gafka := lib.Gafka(ctx, lib.Configuration{
+			BatchSize:       10,
+			ReclaimInterval: time.Second * 2,
+			Topics:          nil,
+		})
 
-	go func() {
-		// advanced test
-		// cancel consumer after 11 seconds
-		time.Sleep(time.Second * 6)
-		unsubscribe()
-	}()
+		gafka.WaitSysNotify()
+		gafka.Shutdown()
+
+		return nil
+	}
+
+	err := application.Run(os.Args)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	go func() {
-		time.Sleep(3 * time.Second)
-
-		err, unsubscribe := gafka.Subscribe(lib.SubscribeConf{
-			Topic: topic,
-			Group: "group1",
-			Handler: func(message lib.ReceiveMessage) {
-				//log.Println("Second")
-				//log.Println(message.Topic)
-				//log.Println(message.Partition)
-				//log.Println(message.Messages)
-			},
-		})
-
-		defer unsubscribe()
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		time.Sleep(1000 * time.Second)
-	}()
-
-	go func() {
-		time.Sleep(5 * time.Second)
-
-		err, unsubscribe := gafka.Subscribe(lib.SubscribeConf{
-			Topic: topic,
-			Group: "group1",
-			Handler: func(message lib.ReceiveMessage) {
-				//log.Println("Third")
-				//log.Println(message.Topic)
-				//log.Println(message.Partition)
-				//log.Println(message.Messages)
-			},
-		})
-
-		defer unsubscribe()
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		time.Sleep(1000 * time.Second)
-	}()
-
-	go func() {
-		time.Sleep(7 * time.Second)
-
-		err, unsubscribe := gafka.Subscribe(lib.SubscribeConf{
-			Topic: topic,
-			Group: "group1",
-			Handler: func(message lib.ReceiveMessage) {
-				//log.Println("Four")
-				//log.Println(message.Topic)
-				//log.Println(message.Partition)
-				//log.Println(message.Messages)
-			},
-		})
-
-		defer unsubscribe()
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		time.Sleep(1000 * time.Second)
-	}()
-
-	go func() {
-		time.Sleep(9 * time.Second)
-
-		err, unsubscribe := gafka.Subscribe(lib.SubscribeConf{
-			Topic: topic,
-			Group: "group1",
-			Handler: func(message lib.ReceiveMessage) {
-				//log.Println("Five")
-				//log.Println(message.Topic)
-				//log.Println(message.Partition)
-				//log.Println(message.Messages)
-			},
-		})
-
-		defer unsubscribe()
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		time.Sleep(1000 * time.Second)
-	}()
-
-	go func() {
-		time.Sleep(11 * time.Second)
-
-		err, unsubscribe := gafka.Subscribe(lib.SubscribeConf{
-			Topic: topic,
-			Group: "group1",
-			Handler: func(message lib.ReceiveMessage) {
-				//log.Println("Six")
-				//log.Println(message.Topic)
-				//log.Println(message.Partition)
-				//log.Println(message.Messages)
-			},
-		})
-
-		defer unsubscribe()
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		time.Sleep(1000 * time.Second)
-	}()
-
-	go func() {
-		time.Sleep(13 * time.Second)
-
-		err, unsubscribe := gafka.Subscribe(lib.SubscribeConf{
-			Topic: topic,
-			Group: "group1",
-			Handler: func(message lib.ReceiveMessage) {
-				//log.Println("Seven")
-				//log.Println(message.Topic)
-				//log.Println(message.Partition)
-				//log.Println(message.Messages)
-			},
-		})
-
-		defer unsubscribe()
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		time.Sleep(1000 * time.Second)
-	}()
-
-	go func() {
-		for i := 0; i <= 500; i++ {
-			if err := gafka.Publish(topic, fmt.Sprintf("message #%d", i)); err != nil {
-				fmt.Println(err)
-			}
-
-			time.Sleep(100 * time.Millisecond)
-		}
-	}()
-
-	gafka.WaitSysNotify()
-	gafka.Shutdown()
 }
